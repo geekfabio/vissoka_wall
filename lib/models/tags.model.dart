69728f7d45e2eed020067a6ec072e3a0b3307d93
geekfabio <geekfabio@gmail.com>
2021-03-15 13:53:33 +0100

    Add Tag Itens

---
+++ lib/models/tags.model.dart
@@ -0,0 +1,8 @@
+class TagModel {
+  int id;
+  String title;
+  String image;
+  String quantity;
+
+  TagModel({this.id, this.title, this.image, this.quantity});
+}
--- lib/utils/mockups/data.mockup.dart
+++ lib/utils/mockups/data.mockup.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:hello_word/models/category.model.dart';
 import 'package:hello_word/models/color.model.dart';
+import 'package:hello_word/models/tags.model.dart';
 
 class MockupData {
   MockupData() {
     getData();
     getColors();
+    getTags();
   }
+
+  List<TagModel> tags = [];
   List<CategoryModel> categories = [];
   List<ColorModel> colors = [];
-  CategoryModel model = new CategoryModel(id: "1", title: "Cat", tag: "animal");
-  ColorModel colorItem =
-      new ColorModel(id: 1, title: "Blue", color: Colors.blue);
 
   getData() {
+    CategoryModel model =
+        new CategoryModel(id: "1", title: "Cat", tag: "animal");
     categories.add(model);
     model = new CategoryModel(id: "2", title: "Cars", tag: "Motor");
     categories.add(model);
@@ -28,23 +31,48 @@ class MockupData {
   }
 
   getColors() {
+    ColorModel colorItem =
+        new ColorModel(id: 1, title: "Blue", color: Colors.blue);
     colorItem =
-        new ColorModel(id: 1, title: "yellow", color: Color(0xFFFFE57F));
+        new ColorModel(id: 2, title: "yellow", color: Color(0xFFFFE57F));
     colors.add(colorItem);
-    colorItem = new ColorModel(id: 1, title: "green", color: Color(0xFFAED581));
+    colorItem = new ColorModel(id: 3, title: "green", color: Color(0xFFAED581));
     colors.add(colorItem);
-    colorItem = new ColorModel(id: 1, title: "Blue", color: Color(0xFF4FC3F7));
+    colorItem = new ColorModel(id: 4, title: "Blue", color: Color(0xFF4FC3F7));
     colors.add(colorItem);
     colorItem =
-        new ColorModel(id: 1, title: "orange", color: Color(0xFFFFCC80));
+        new ColorModel(id: 5, title: "orange", color: Color(0xFFFFCC80));
     colors.add(colorItem);
-    colorItem = new ColorModel(id: 1, title: "pink", color: Color(0xFFF48FB1));
+    colorItem = new ColorModel(id: 6, title: "pink", color: Color(0xFFF48FB1));
     colors.add(colorItem);
 
-    colorItem = new ColorModel(id: 1, title: "black", color: Color(0xFF263238));
+    colorItem = new ColorModel(id: 7, title: "black", color: Color(0xFF263238));
     colors.add(colorItem);
 
-    return categories;
+    return colors;
     //TODO change Colors.name TO Colors.fromArgb("") Na Model tem que receber uma string e converter em Color
+  }
+
+  getTags() {
+    TagModel item = new TagModel(
+        id: 1, title: "Anime", image: "assets/img/1.jpg", quantity: "+300");
+    tags.add(item);
+    item = new TagModel(
+        id: 2, title: "Otakus", image: "assets/img/2.jpg", quantity: "+760");
+    tags.add(item);
+    item = new TagModel(
+        id: 3,
+        title: "Developers",
+        image: "assets/img/3.jpg",
+        quantity: "+200");
+    tags.add(item);
+    item = new TagModel(
+        id: 4, title: "Hackers", image: "assets/img/4.jpg", quantity: "+379");
+    tags.add(item);
+    item = new TagModel(
+        id: 5, title: "Sexy", image: "assets/img/4.jpg", quantity: "+678");
+    tags.add(item);
+
+    return tags;
   }
 }
